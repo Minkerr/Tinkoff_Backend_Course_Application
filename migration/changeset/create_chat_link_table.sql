40cbb5bbb1141768a8b7f9fd53691d9dc8491698
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-03-11 02:14:07 +0300

    fix the error

--- migration/changeset/create_chat_link_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS chat_links
-(
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
-    id_chat BIGINT NOT NULL REFERENCES chats (id),
-    id_link BIGINT NOT NULL REFERENCES links (id),
-
-    PRIMARY KEY(id),
-);
---
+++ migrations/changeset/create_chat_link_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE chat_links
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    id_chat BIGINT NOT NULL REFERENCES chats (id),
+    id_link BIGINT NOT NULL REFERENCES links (id)
+);
