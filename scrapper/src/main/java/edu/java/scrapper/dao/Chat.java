152fe8069068c492cb3232eb1a81a0746e51f359
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-03-16 21:29:04 +0300

    init commit with dao

---
+++ scrapper/src/main/java/edu/java/scrapper/dao/Chat.java
@@ -0,0 +1,5 @@
+package edu.java.scrapper.dao;
+
+public class Chat {
+
+}
---
+++ scrapper/src/main/java/edu/java/scrapper/dao/Link.java
@@ -0,0 +1,38 @@
+package edu.java.scrapper.dao;
+import java.time.OffsetDateTime;
+
+public class Link {
+    long id;
+    String url;
+    OffsetDateTime lastUpdated;
+
+    public Link(long id, String url) {
+        this.id = id;
+        this.url = url;
+        this.lastUpdated = OffsetDateTime.now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public OffsetDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(OffsetDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+}
