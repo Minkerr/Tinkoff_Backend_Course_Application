f792d14676180dd7ab9e5b7305edf5cc2ea1b3e9
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-03-22 21:53:06 +0300

    prepare for start fully working bot

--- scrapper/src/main/java/edu/java/scrapper/service/ScrapperService.java
+++ scrapper/src/main/java/edu/java/scrapper/service/ScrapperService.java
@@ -5,36 +5,36 @@ import edu.java.scrapper.dto.ListLinksResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ScrapperService {
-    private final Map<Long, ListLinksResponse> chatsWithLinks = new HashMap<>();
+    private final ChatService chatService;
+
+    @Autowired
+    public ScrapperService(ChatService chatService) {
+        this.chatService = chatService;
+    }
 
     public void registerChat(long chatId) {
-        chatsWithLinks.put(chatId, new ListLinksResponse(0, new ArrayList<>()));
+
     }
 
     public void deleteChat(Long chatId) {
-        chatsWithLinks.remove(chatId);
+
     }
 
     public ListLinksResponse getLinks(Long chatId) {
-        return chatsWithLinks.get(chatId);
+        return null;
     }
 
     public LinkResponse addLink(Long chatId, LinkResponse link) {
-        ListLinksResponse linkResponses = chatsWithLinks.get(chatId);
-        linkResponses.addLinkResponse(link);
-        chatsWithLinks.put(chatId, linkResponses);
 
         return link;
     }
 
     public LinkResponse deleteLink(Long chatId, LinkResponse link) {
-        ListLinksResponse linkResponses = chatsWithLinks.get(chatId);
-        linkResponses.deleteLinkResponse(link);
-        chatsWithLinks.put(chatId, linkResponses);
 
         return link;
     }
